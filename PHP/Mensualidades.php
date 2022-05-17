b95933e514a6ee7e1ee52b6f1e434e6059d7a18c
Ricardo ZP <pzrico@hotmail.com>
2022-05-17 09:46:00 -0500

    Update Mensualidades.php

--- PHP/Mensualidades.php
+++ PHP/Mensualidades.php
@@ -1,17 +1,17 @@
 <?php
-$a = 12;
-$b = 24965.968;
+$a = 12; //mensualidades
+$b = 24965; //Monto
 $suma = 0;
 $c = intdiv($b, $a);
 $d = $b % $a;
 for ($i = 1; $i <= 12; $i++) {
     if ($i == 12) {
         $res = $c + $d;
-        echo "La menualidad ".$i." es: ".$res."\n";
+        echo "La mensualidad ".$i." es: ".$res."\n";
     }
     else {
         $res = $c;
-        echo "La menualidad ".$i." es: ".$res."\n";
+        echo "La mensualidad ".$i." es: ".$res."\n";
     }
     $suma = $suma + $res;
 }
